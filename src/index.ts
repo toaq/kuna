5867233b124df9b707613bbaa56e85e2dd35782d
Lynn <lynn@foldr.moe>
2023-01-27 17:57:04 +0100

    Add yargs cli

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,43 @@ import { SAP } from './parse';
 import { lexer, preprocess } from './tokenize';
 import * as fs from 'fs';
 import { alignGlossSentence } from './gloss';
+import yargs from 'yargs';
 
-console.log(alignGlossSentence('Fạchuq. Ꝡa chum leo fıeq jí sá bu-nıq da.'));
-
-const tree = expectSingleResult(
-	expectEOF(
-		SAP.parse(preprocess(lexer.parse('Ꝡa chum leo fıeq jí sá nıq da')!)),
-	),
-);
-
-fs.writeFileSync('output.tex', toDocument(tree));
+yargs
+	.scriptName('kuna')
+	.usage('$0 <mode> --sentence "Hıo ka"')
+	.option('sentence', {
+		type: 'string',
+		describe: 'The Toaq sentence to parse',
+	})
+	.command(
+		'gloss-ascii',
+		'Gloss to aligned ASCII format',
+		yargs => {
+			yargs.demandOption('sentence');
+		},
+		function (argv) {
+			console.log(alignGlossSentence(argv.sentence!));
+		},
+	)
+	.command(
+		'stree',
+		'Save surface-level grammar tree to LaTeX',
+		yargs => {
+			yargs.demandOption('sentence');
+			yargs.option('output', {
+				type: 'string',
+				describe: 'Path for LaTeX output',
+				default: 'output.tex',
+			});
+		},
+		function (argv: any) {
+			const tree = expectSingleResult(
+				expectEOF(SAP.parse(preprocess(lexer.parse(argv.sentence)!))),
+			);
+			fs.writeFileSync(argv.output, toDocument(tree));
+		},
+	)
+	.strict()
+	.demandCommand()
+	.help().argv;
