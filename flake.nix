f7a3fe184c81637a9d844e9b01a8ec0dfd5e46eb
Robin Townsend <robin@robin.town>
2023-07-23 11:45:56 -0400

    Add a Nix dev shell

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.flake-utils.url = github:numtide/flake-utils;
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default =
+          pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs_latest
+              nodePackages_latest.typescript-language-server
+              nodePackages_latest.vscode-json-languageserver
+            ];
+          };
+      });
+}
