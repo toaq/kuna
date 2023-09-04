3d5660b551cb2a40eb9820820055a61c9f0a438c
Lynn <lynn@foldr.moe>
2023-09-05 00:24:48 +0200

    Set base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import requireTransform from 'vite-plugin-require-transform';
 
 export default defineConfig({
+	base: '/kuna/',
 	plugins: [react(), tsconfigPaths(), requireTransform({})],
 });
