4d2e938cd153f65671df22d4df2f65d535ba68fa
uakci <git@uakci.space>
2023-09-06 11:47:51 +0200

    nixos flake: format with nixfmt

--- flake.nix
+++ flake.nix
@@ -1,25 +1,24 @@
 {
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+      let pkgs = import nixpkgs { inherit system; };
       in {
-        devShells.default =
-          pkgs.mkShell {
-            buildInputs = with pkgs; [
-              nodejs_latest
-              nodePackages_latest.typescript-language-server
-              nodePackages_latest.vscode-json-languageserver
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_latest
+            nodePackages_latest.typescript-language-server
+            nodePackages_latest.vscode-json-languageserver
 
-              # Dependencies for building canvas
-              python3
-              pkg-config
-              pixman
-              cairo
-              pango
-            ];
+            # Dependencies for building canvas
+            python3
+            pkg-config
+            pixman
+            cairo
+            pango
+          ];
 
-            env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [ libuuid ]);
-          };
+          env.LD_LIBRARY_PATH =
+            pkgs.lib.makeLibraryPath (with pkgs; [ libuuid ]);
+        };
       });
 }
