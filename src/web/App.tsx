efbfabce1c04077a3ab6d09bf85853c9ec827f0d
Lynn <lynn@foldr.moe>
2023-12-08 18:21:35 +0100

    web: Use react-router for routing

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react';
 import { useDarkMode } from 'usehooks-ts';
 import { Main } from './Main';
 import { Sentences } from './Sentences';
 import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
 
 export function App() {
 	const darkMode = useDarkMode();
-	const tools = ['Interactive', 'Sentences'];
-	const [tool, setTool] = useState('Interactive');
 
 	return (
 		<div className={darkMode.isDarkMode ? 'kuna dark-mode' : 'kuna'}>
 			<header>
 				<h1>mí Kuna</h1>
-				<div style={{ marginLeft: 10 }}>
-					{tools.map(t => (
-						<button
-							key={t}
-							className={tool === t ? 'current' : ''}
-							id={t}
-							onClick={() => setTool(t)}
-						>
-							{t}
-						</button>
-					))}
-				</div>
+				<nav style={{ marginLeft: 10 }}>
+					<NavLink to="/">Interactive</NavLink>
+					<NavLink to="/sentences">Sentences</NavLink>
+				</nav>
 				<button style={{ marginLeft: 'auto' }} onClick={darkMode.toggle}>
 					Toggle theme
 				</button>
 			</header>
-			{tool === 'Sentences' ? <Sentences /> : <Main />}
+			<Routes>
+				<Route path="/" element={<Main />} />
+				<Route path="/sentences" element={<Sentences />} />
+			</Routes>
 		</div>
 	);
 }
