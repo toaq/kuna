a0aca9ee793273a8fac5c9a6d402a6a645378cdb
Lynn <lynn@foldr.moe>
2024-05-02 17:55:33 +0200

    Fix regexes and reduce Unicode sins in guessFrameFromDefinition

--- src/frame.test.ts
+++ src/frame.test.ts
@@ -7,5 +7,7 @@ test('it makes frames from Toadua definitions', () => {
     expect(guessFrameFromDefinition('▯ is one thing; ▯ is another thing.')).toEqual('c');
 	expect(guessFrameFromDefinition('▯ tells ▯ that ▯ is the case.')).toEqual('c c 0');
 	expect(guessFrameFromDefinition('▯ tests out satisfying property ▯.')).toEqual('c 1');
+	expect(guessFrameFromDefinition('▯ tests out satisfying ▯.')).toEqual('c 1');
+	expect(guessFrameFromDefinition('▯ is less than ▯ in property ▯.')).toEqual('c c 1');
 	expect(guessFrameFromDefinition('That ▯ is the case saddens ▯.')).toEqual('0 c');
 });
--- src/frame.ts
+++ src/frame.ts
@@ -4,12 +4,13 @@ export function guessFrameFromDefinition(definition: string): string {
 	const frame = definition
 		.split(';')[0]
 		.toLowerCase()
-		.replace(/▯ (\S+ ){0,2}the case/g, '０')
-		.replace(/satisf\w+ (property ?)▯/g, '１')
-		.replace(/relation ▯/g, '２')
-		.replace(/▯/g, 'ｃ')
-		.replace(/[^０１２ｃ]/g, '')
-		.normalize('NFKC');
+		.replace(/\d/g, '')
+		.replace(/▯ (\S+ ){0,2}the case/g, '0')
+		.replace(/satisf\w+ (property )?▯/g, '1')
+		.replace(/property ▯/g, '1')
+		.replace(/relation ▯/g, '2')
+		.replace(/[^012▯]/g, '')
+		.replace(/▯/g, 'c');
 	return [...frame].join(' ');
 }
 
