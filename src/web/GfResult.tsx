c417bb0fe7adba15a2f41f5cdaa1a7e90d124eae
Lynn <lynn@foldr.moe>
2024-06-12 01:58:32 +0200

    Show errors from GF API

--- src/web/GfResult.tsx
+++ src/web/GfResult.tsx
@@ -29,18 +29,28 @@ export interface GfResultProps {
 
 export default (props: GfResultProps) => {
 	const [linearizations, setLinearizations] = useState<Linearization[]>([]);
+	const [error, setError] = useState('');
 	const { gf } = props;
 	useEffect(() => {
 		fetch(
 			'https://cloud.grammaticalframework.org/grammars/ResourceDemo.pgf?command=linearize&tree=' +
 				encodeURIComponent(gf),
-		).then(response => response.json().then(json => setLinearizations(json)));
+		).then(response => {
+			if (response.status >= 400) {
+				setLinearizations([]);
+				response.text().then(error => setError(error));
+			} else {
+				setError('');
+				response.json().then(json => setLinearizations(json));
+			}
+		});
 	}, [gf]);
 	return (
 		<div>
 			<div style={{ maxWidth: '66ch' }}>
 				<code>{gf}</code>
 			</div>
+			{error && <pre style={{ color: '#d00000' }}>{error}</pre>}
 			<ul>
 				{linearizations.map((l, i) => (
 					<ShowLinearization linearization={l} key={i} />
